9b4d7e2145cddffcf8e1b77a30ff3ca3e2ad4421
KJoaquinklein <joa_klein1@hotmail.com>
2025-10-08 22:15:12 -0300

    paso a note

--- src/App.jsx
+++ src/App.jsx
@@ -40,12 +40,14 @@ import { FaCss3Alt } from "react-icons/fa"; //<FaCss3Alt />
 import { FaReact } from "react-icons/fa"; //<FaReact />
 import { SiRedux } from "react-icons/si"; //<SiRedux />
 import { RiTailwindCssFill } from "react-icons/ri"; //<RiTailwindCssFill />
+import { IoLogoElectron } from "react-icons/io5";
 
 //todo BACK
 import { FaNodeJs } from "react-icons/fa"; //<FaNodeJs />
 import { SiExpress } from "react-icons/si"; //<SiExpress />
 import { SiSequelize } from "react-icons/si"; //<SiSequelize />
 import { BiLogoPostgresql } from "react-icons/bi"; //<BiLogoPostgresql />
+import { SiSqlite } from "react-icons/si";
 
 //todo APRENDIENDO
 import { SiNextdotjs } from "react-icons/si"; //<SiNextdotjs />
@@ -91,7 +93,7 @@ function App() {
     return (
         <div className="overflow-hidden">
             {/* HOME **************************************************************************** */}
-            <div className="bg-black-0 min-h-screen max-w-screen flex flex-col items-center justify-between ">
+            {/* <div className="bg-black-0 min-h-screen max-w-screen flex flex-col items-center justify-between ">
                 <div
                     className="bg-green-0 flex mv:flex-col md:flex-row mv:h-[620px] md:w-[650px] lg:w-[850px] mv:w-[300px] md:h-[300px] lg:h-[400px] p-10 font-mono mv:mt-11 md:mt-40 mv:pb-5 md:pb-10"
                     data-aos="fade-down"
@@ -107,18 +109,17 @@ function App() {
                     <div className="flex flex-col justify-between mv:items-center md:items-start mv:mt-2 mv:ml-0 md:mt-0 md:ml-10">
                         <div className="text-black-0">
                             <h1 className="mv:text-3xl md:text-3xl lg:text-5xl font-semibold">Joaquín Klein</h1>
-                            <p className="mv:text-xl md:text-xl lg:text-3xl mt-2 font-semibold">Frontend Developer</p>
-                            <p className="md:text-sm lg:text-xl mt-2 text-purple-0 font-semibold">
-                                Entusiasta del diseño y de la creación de experiencias digitales.
+                            <p className="mv:text-xl md:text-xl lg:text-3xl mt-2 font-semibold">
+                                Desarrollador Frontend
                             </p>
                         </div>
 
-                        <div className="flex mv:mb-5 sm:mb-0 mv:flex-col md:flex-row justify-between w-full text-center">
+                        <div className="flex mv:mb-5 sm:mb-0 mv:flex-col md:flex-row justify-between w-full text-center space-x-2">
                             <a href="#aboutMe" className="button">
                                 Sobre mí
                             </a>
                             <a href="#proyects" className="button">
-                                Proyectos
+                                Proyectos y Trabajos
                             </a>
                             <a href="#contact" className="button">
                                 Contacto
@@ -132,24 +133,24 @@ function App() {
                 >
                     <FaArrowDown className="text-xl" />
                 </a>
-            </div>
+            </div> */}
 
             {/* ABOUT ME **************************************************************************** */}
             <div
                 id="aboutMe"
-                className="bg-white-0 max-w-screen text-black-0 font-mono pt-10 pb-20 mv:px-4 sm:px-10 xl:px-60"
+                className="bg-white-0 max-w-screen text-black-0 font-mono pt-10 pb-5 mv:px-4 sm:px-10 xl:px-60"
             >
                 <h2 className="font-semibold text-4xl" data-aos="fade-right">
-                    Sobre mí
+                    Hola! mi nombre es Joaquín Klein 👋🏼
                 </h2>
                 <p className="mv:ml-5 sm:ml-20 mt-5 mv:text-sm sm:text-xl md:text-2xl" data-aos="fade-right">
-                    Desarrollador Frontend y compositor musical. Comencé mis estudios en tecnóloga como autodidacta,
-                    aprendiendo las bases del desarrollo web, conocimientos que amplié en mi paso por distintos cursos.
-                    Siempre estoy incorporando nuevas tecnologías y profundizando en las que ya conozco para mejorar mis
-                    habilidades.
+                    Soy desarrollador frontend y de sistemas de gestión. Comencé mis estudios en tecnóloga como
+                    autodidacta, aprendiendo las bases del desarrollo web, conocimientos que amplié en mi paso por
+                    distintos cursos. Actualmente me encuentro desarrollando distintos sistemas de gestión para
+                    comercios locales y pequeñas empresas.
                 </p>
                 <h3 className="font-semibold sm:text-3xl md:text-4xl mt-10" data-aos="fade-right">
-                    Stack de tecnologías
+                    Stack de tecnologías 👨🏻‍💻
                 </h3>
                 <div className="mt-5">
                     <div className="flex md:ml-14 xl:ml-20" data-aos="fade-left">
@@ -190,6 +191,10 @@ function App() {
                                 <p className="mv:hidden sm:flex sm:text-md md:text-xl">Astro</p>
                                 <SiAstro className="sm:text-2xl md:text-4xl mv:ml-0 sm:ml-2" />
                             </div>
+                            <div className="cardFront">
+                                <p className="mv:hidden sm:flex sm:text-md md:text-xl">Electron</p>
+                                <IoLogoElectron className="sm:text-2xl md:text-4xl mv:ml-0 sm:ml-2" />
+                            </div>
                         </div>
                     </div>
                     <div className="flex items-center mt-4 md:ml-14 xl:ml-20" data-aos="fade-left">
@@ -214,10 +219,15 @@ function App() {
                                 <p className="mv:hidden sm:flex sm:text-md md:text-xl">PostgreSQL</p>
                                 <BiLogoPostgresql className="sm:text-2xl md:text-4xl mv:ml-0 sm:ml-2" />
                             </div>
+                            <div className="cardBack">
+                                <p className="mv:hidden sm:flex sm:text-md md:text-xl">SQLite</p>
+                                <SiSqlite className="sm:text-2xl md:text-4xl mv:ml-0 sm:ml-2" />
+                            </div>
                         </div>
                     </div>
-                    <div className="flex items-center mt-4 md:ml-14 xl:ml-20" data-aos="fade-left">
-                        {/* ACTUAL */}
+
+                    {/* <div className="flex items-center mt-4 md:ml-14 xl:ml-20" data-aos="fade-left">
+             
                         <p className="font-semibold  sm:text-xl md:text-2xl mt-4">Actualmente estoy aprendiendo:</p>
                         <div className="cardActual">
                             <p className="mv:hidden sm:flex sm:text-md md:text-xl">Next</p>
@@ -226,88 +236,32 @@ function App() {
                         <div className="cardActual">
                             <p className="mv:hidden sm:flex sm:text-md md:text-xl">Socket.io</p>
                             <SiSocketdotio className="sm:text-2xl md:text-4xl mv:ml-0 sm:ml-2" />
-                        </div>
-                        <div className="cardActual">
+                            </div>
+                            <div className="cardActual">
                             <p className="mv:hidden sm:flex sm:text-md md:text-xl">Angular</p>
                             <FaAngular className="sm:text-2xl md:text-4xl mv:ml-0 sm:ml-2" />
-                        </div>
-                    </div>
+                            </div>
+                        </div> */}
+                </div>
+                <div className="w-full flex justify-center pt-14">
+                    <a
+                        href="#proyectos_y_trabajos"
+                        className="mv:p-6 md:p-4 xl:p-6 bg-black-0 rounded-full shadow-xl animate-bounce"
+                    >
+                        <FaArrowDown className="text-x text-white-0" />
+                    </a>
                 </div>
             </div>
 
             {/* PROYECTS ****************************************************************************** */}
             <div
-                id="proyects"
+                id="proyectos_y_trabajos"
                 className="bg-purple-0 max-w-screen text-white-0 font-mono pt-10 pb-20  mv:px-4 sm:px-10 xl:px-60"
             >
                 <h2 className=" text-4xl" data-aos="fade-right">
-                    Proyectos
+                    Proyectos y Trabajos 📁
                 </h2>
                 <div className="mt-5 mv:px-4 sm:px-10 flex flex-col items-center" data-aos="fade-left">
-                    {/* MI CANCIONERO */}
-                    <div
-                        className={
-                            showMoreMC
-                                ? "bg-black-0 p-4 flex flex-col max-w-[1160px]"
-                                : "bg-black-0 p-4 flex items-center max-w-[1160px]"
-                        }
-                    >
-                        <img
-                            src={MCHome}
-                            alt="Mi cancionero Home"
-                            className={showMoreMC ? "hidden" : "md2:flex mv:hidden h-[160px] w-[305px]"}
-                        />
-                        <div className="ml-3">
-                            <div className="flex flex-col h-[160px] justify-between">
-                                <div>
-                                    <div className="flex justify-between">
-                                        <h3 className="font-semibold mv:text-lg sm:text-xl md:text-2xl mb-1">
-                                            Mi Cancionero
-                                        </h3>
-                                        <div className="flex text-2xl text-green-0">
-                                            <a
-                                                href="https://mi-cancionero.onrender.com/"
-                                                className="hover:text-purple-0 duration-100"
-                                                target="_blanck"
-                                            >
-                                                <FaRocket className="mr-5" />
-                                            </a>
-                                            <a
-                                                href="https://github.com/KJoaquinklein/miCancioneroFRONT"
-                                                className="hover:text-purple-0 duration-100"
-                                                target="_blanck"
-                                            >
-                                                <FaGithub />
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <p className="mv:text-xs sm:text-sm md:text-md">
-                                        Cancionero personal que consta de tres secciones, una donde el usuario puede ver
-                                        las canciones registradas, otra donde le permite cargar una nueva canción, y por
-                                        ultimo donde puede reproducir la letra para ensayar.
-                                    </p>
-                                </div>
-                                <div className="flex justify-between">
-                                    <div className="flex text-2xl space-x-2">
-                                        <BiLogoTypescript className="text-purple-0" />
-                                        <FaReact className="text-purple-0" />
-                                        <SiRedux className="text-purple-0" />
-                                        <RiTailwindCssFill className="text-purple-0" />
-                                        <SiExpress className="text-green-0" />
-                                    </div>
-                                    <button className="text-green-0 hover:underline" onClick={handleMC}>
-                                        {showMoreMC ? <>Ver menos</> : <>Ver más</>}
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={showMoreMC ? "flex overflow-auto justify-between mt-4" : "hidden"}>
-                            <img src={MCHome} className="h-36 mr-2" />
-                            <img src={MC2} className="h-36 mr-2" />
-                            <img src={MC3} className="h-36 mr-2" />
-                        </div>
-                    </div>
-
                     {/* OPEN BOOK */}
                     <div
                         className={
@@ -369,63 +323,6 @@ function App() {
                             <img src={OBHome} className="h-36 mr-2" />
                             <img src={OB2} className="h-36 mr-2" />
                             <img src={OB3} className="h-36 mr-2" />
-                        </div>
-                    </div>
-
-                    {/* COUNTRY APP */}
-                    <div
-                        className={
-                            showMoreCA
-                                ? "bg-black-0 p-4 flex flex-col mt-4 max-w-[1160p[x]"
-                                : "bg-black-0 p-4 flex mt-4 max-w-[1160px]"
-                        }
-                    >
-                        <img
-                            src={CAHome}
-                            alt="Country app Home"
-                            className={showMoreCA ? "hidden" : "md2:flex mv:hidden h-[160px] w-[305px]"}
-                        />
-                        <div className="ml-3  h-[160px] flex flex-col justify-between">
-                            <div>
-                                <div className="flex  justify-between">
-                                    <h3 className="font-semibold  mv:text-lg sm:text-xl md:text-2xl mb-1">
-                                        Country app
-                                    </h3>
-                                    <div className="flex text-2xl text-green-0">
-                                        <a
-                                            href="https://github.com/KJoaquinklein/PI-countries-Joaquin-Klein"
-                                            className="hover:text-purple-0 duration-100"
-                                            target="_blanck"
-                                        >
-                                            <FaGithub />
-                                        </a>
-                                    </div>
-                                </div>
-                                <p className="mv:text-xs sm:text-sm md:text-md">
-                                    Aplicación donde podemos ver información y actividades turísticas de cada país, como
-                                    también crear una actividad turística para uno o mas paises. También podemos
-                                    filtrar, ordenar los paises o buscarlo por su nombre.
-                                </p>
-                            </div>
-                            <div className="flex justify-between">
-                                <div className="flex text-2xl space-x-2 flex-wrap">
-                                    <BiLogoJavascript className="text-purple-0" />
-                                    <FaReact className="text-purple-0" />
-                                    <SiRedux className="text-purple-0" />
-                                    <FaCss3Alt className="text-purple-0" />
-                                    <SiExpress className="text-green-0" />
-                                    <SiSequelize className="text-green-0" />
-                                    <BiLogoPostgresql className="text-green-0" />
-                                </div>
-                                <button className="text-green-0 hover:underline" onClick={handleCA}>
-                                    {showMoreCA ? <>Ver menos</> : <>Ver más</>}
-                                </button>
-                            </div>
-                        </div>
-                        <div className={showMoreCA ? "flex overflow-auto  justify-between mt-4" : "hidden"}>
-                            <img src={CAHome} className="h-36 mr-2" />
-                            <img src={CA2} className="h-36 mr-2" />
-                            <img src={CA3} className="h-36 mr-2" />
                         </div>
                     </div>
 
@@ -495,7 +392,7 @@ function App() {
                 className="bg-green-0 max-w-screen text-black-0 font-mono pt-10  mv:px-4 sm:px-10 xl:px-60 font-semibold"
             >
                 <h2 className="text-4xl" data-aos="fade-right">
-                    Contacto
+                    Contacto ☎️
                 </h2>
                 <div className="flex mv:mx-4 md:mx-20 mt-4 justify-center flex-wrap" data-aos="fade-left">
                     <a
